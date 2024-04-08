128dc8a4115bbb99504ab2ec8e901be9e2a774e0
eladkapuza <eladkapuza@gmail.com>
2024-04-08 18:18:20 +0300

    updated vertex neighbors field

--- js/vertex.js
+++ js/vertex.js
@@ -3,13 +3,15 @@ class Vertex {
     this.row = row;
     this.col = col;
     this.domElement = domElement;
-    this.label = label;
     this.visited = false;
     this.focused = false;
     this.chosen = false;
+    this.fValue = 0;
+    this.gValue = 0;
+    this.hValue = 0;
     this.parent = null;
     this.distance = 0;
-    this.neighbors = { right: null };
+    this.neighbors = { right: null, left: null, top: null, bottom: null };
   }
 
   setVisited(bool) {
